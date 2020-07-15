510e51cc5b79a3d2c76b0eb78aff404e717fc391
Danya <6694524@gmail.com>
2020-07-15 19:22:20 +0300

    create hostel

--- components/header.js
+++ components/header.js
@@ -1,48 +1,50 @@
 'use strict';
 
-// const header = () => {
-//     const netWorks = document.querySelector('.header-netWorks'),
-//         listItem = document.querySelector('.header-list'),
-//         menuLink = document.querySelector('.header-menuLink'),
-//         menuBlock = document.querySelector('.menuBlock'),
-//         navigation = document.querySelector('.navigation'),
-//         listItemLinks = document.querySelectorAll('.header-listItem-links'),
-//         header = document.querySelector('.header')
-//     const laptopChanges = () => {
-//         if (laptop.matches) {
-//             menuLink.style.display = 'flex';
-//             menuBlock.append(netWorks)
-//         } else {
-//             header.appendChild(netWorks);
-//             menuLink.style.display = 'none';
-//         }
-//     }
+const header = () => {
+    const netWorks = document.querySelector('.header-netWorks'),
+        listItem = document.querySelector('.header-list'),
+        menuLink = document.querySelector('.header-menuLink'),
+        menuBlock = document.querySelector('.menuBlock'),
+        navigation = document.querySelector('.navigation'),
+        header = document.querySelector('.header'),
+        blackout = document.querySelector('.blackout');
+    const laptopChanges = () => {
+        if (laptop.matches) {
+            menuLink.style.display = 'flex';
+            menuBlock.append(netWorks)
+        } else {
+            header.appendChild(netWorks);
+            menuLink.style.display = 'none';
+        }
+    }
 
-//     const tabletChanges = () => {
-//         if (tablet.matches) {
-//             menuBlock.append(listItem)
-//         } else {
-//             navigation.append(listItem)
-//         }
-//     }
+    const tabletChanges = () => {
+        if (tablet.matches) {
+            menuBlock.append(listItem)
+        } else {
+            navigation.append(listItem)
+        }
+    }
 
-//     const tablet = window.matchMedia('(max-width: 855px)');
-//     tabletChanges(tablet);
-//     tablet.addListener(tabletChanges);
+    const tablet = window.matchMedia('(max-width: 855px)');
+    tabletChanges(tablet);
+    tablet.addListener(tabletChanges);
 
-//     const laptop = window.matchMedia("(max-width: 1024px)");
-//     laptopChanges(laptop);
-//     laptop.addListener(laptopChanges);
+    const laptop = window.matchMedia("(max-width: 1024px)");
+    laptopChanges(laptop);
+    laptop.addListener(laptopChanges);
 
-//     menuLink.addEventListener('click', (e) => {
-//         e.preventDefault();
-//         menuLink.classList.toggle('activeMenuLink');
-//         if (menuLink.classList.contains('activeMenuLink')) {
-//             menuBlock.classList.add('menuBlockActive');
-//         } else {
-//             menuBlock.classList.remove('menuBlockActive');
-//         }
-//     })
-// }
+    menuLink.addEventListener('click', (e) => {
+        e.preventDefault();
+        menuLink.classList.toggle('activeMenuLink');
+        if (menuLink.classList.contains('activeMenuLink')) {
+            menuBlock.classList.add('menuBlockActive');
+            blackout.style.display = 'block';
+        } else {
+            menuBlock.classList.remove('menuBlockActive');
+            blackout.style.display = 'none';
+        }
+    })
+}
 
 export default header;
