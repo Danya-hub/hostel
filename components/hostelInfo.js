333d9b3f770b04b280e835c4654dac37f92252a2
Danya <6694524@gmail.com>
2020-07-17 23:20:46 +0300

    create hostel

--- components/hostelInfo.js
+++ components/hostelInfo.js
@@ -5,6 +5,8 @@ const hostelInfo = () => {
     const blockInfoTxt = document.querySelector('.hostelInfo-blockInfo-txt'),
         blockInfo = document.querySelector('.hostelInfo-blockInfo');
 
+    const wordCount = 700;
+    blockInfo.innerHTML = blockInfoTxt.textContent.split('').slice(0, wordCount).join('') + '...';
 }
 
 export default hostelInfo;
