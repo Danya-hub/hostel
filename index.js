13de0097dbedace867acfa14cf23a71e4211c3f8
Danya <6694524@gmail.com>
2020-07-22 16:50:23 +0300

    create hostel

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import header from "./components/header.js";
 import hostelInfo from "./components/hostelInfo.js";
 import animationScroll from "./components/animationPage.js";
 
+console.log(document.cookie);
+
 header();
 hostelInfo();
 animationScroll();
