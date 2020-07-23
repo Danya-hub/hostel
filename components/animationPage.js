ba5c8168978b391cdf6f18c244f81a22dd720495
Danya <6694524@gmail.com>
2020-07-23 22:13:19 +0300

    create hostel

--- components/animationPage.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const animationScroll = () => {
-    // const pageWrapper = document.querySelector('.page-wrapper');
-}
-
-export default animationScroll;
--- index.js
+++ index.js
@@ -2,8 +2,26 @@
 
 import header from "./components/header.js";
 import hostelInfo from "./components/hostelInfo.js";
-import animationScroll from "./components/animationPage.js";
 
 header();
 hostelInfo();
-animationScroll();
+
+window.addEventListener('scroll', () => {
+    const scroll = window.pageYOffset;
+    const buttonUp = document.querySelector('.buttonUp'),
+        hostelInfoWrapper = document.querySelector('.hostelInfo-wrapper')
+
+    console.log(scroll);
+    if (scroll !== 0) {
+        buttonUp.style.visibility = 'visible';
+        buttonUp.style.opacity = 0.4;
+    } else {
+        buttonUp.style.visibility = 'hidden';
+        buttonUp.style.opacity = 0;
+    }
+
+    if (scroll >= 300) {
+        hostelInfoWrapper.style.transform = 'translateY(0px)';
+        hostelInfoWrapper.style.opacity = '1';
+    } 
+})
