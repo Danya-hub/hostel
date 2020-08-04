45416d947605ae62edfa5d5bceb444d275e58e85
Danya <6694524@gmail.com>
2020-08-04 11:21:00 +0300

    create site hostel

--- index.js
+++ index.js
@@ -8,12 +8,7 @@ hostelInfo();
 
 window.addEventListener('scroll', () => {
     const scroll = window.pageYOffset || document.documentElement.scrollTop;
-    const buttonUp = document.querySelector('.buttonUp'),
-        hostelInfoWrapper = document.querySelector('.hostelInfo-wrapper'),
-        priceResidenceMainTitle = document.querySelector('.priceResidence-mainTitle'),
-        priceResidenceWrapper = document.querySelector('.priceResidence-wrapper'),
-        contactsMainTitle = document.querySelector('.contacts-mainTitle'),
-        contactsWrapper = document.querySelector('.contacts-wrapper'); 
+    const buttonUp = document.querySelector('.buttonUp');
 
     if (scroll !== 0) {
         buttonUp.style.visibility = 'visible';
@@ -21,30 +16,5 @@ window.addEventListener('scroll', () => {
     } else {
         buttonUp.style.visibility = 'hidden';
         buttonUp.style.opacity = 0;
-    }
-
-    if (scroll > 300) {
-        hostelInfoWrapper.classList.add('translateActive')
-        hostelInfoWrapper.classList.remove('invisible');
-    } 
-    
-    if (scroll > 900) {
-        priceResidenceMainTitle.classList.add('translateActive')
-        priceResidenceMainTitle.classList.remove('invisible');
-    }
-
-    if (scroll > 1000) {
-        priceResidenceWrapper.classList.add('translateActive')
-        priceResidenceWrapper.classList.remove('invisible');
-    }
-
-    if (scroll > 1450) {
-        contactsMainTitle.classList.add('translateActive')
-        contactsMainTitle.classList.remove('invisible');
-    }
-
-    if (scroll > 1550) {
-        contactsWrapper.classList.add('translateActive')
-        contactsWrapper.classList.remove('invisible');
     }
 })
